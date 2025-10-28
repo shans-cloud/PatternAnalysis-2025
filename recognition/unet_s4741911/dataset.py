c35d2b16b4594c066ff4fb25c2c6e2afcce72428
Shannon <shannon.delapena552@gmail.com>
2025-10-28 12:30:01 +1000

    Fix: correct prostate label mapping and tensor transform order
    - Changed prostate mask to correctly identify prostate region (label 5)
    - Added .permute(1, 2, 0) for (C, H, W) format
    - Removed ImageNet normalisation to preserve grayscale MRI intensity
    - Removed redundant tensor casts
    - Cleaned imports and removed visualisation

--- recognition/unet_s4741911/dataset.py
+++ recognition/unet_s4741911/dataset.py
@@ -1,6 +1,6 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
-from utils import load_data_2D, show_examples
+from torch.utils.data import Dataset
+from utils import load_data_2D
 import os
 import numpy as np
 import torchvision.transforms as transforms
@@ -79,50 +79,15 @@ class HipMRIProstateDataset(Dataset):
         if seg.ndim == 2:
             seg = seg[np.newaxis, :, :]
 
-        image = torch.tensor(image, dtype=torch.float32)
-        seg = torch.tensor(seg, dtype=torch.uint8)
-
         if self.transform:
             image = self.transform(image)
 
-        seg_np = seg.numpy()
-        binary_seg = np.zeros_like(seg_np, dtype=np.uint8)
-        binary_seg[seg_np == 1] = 1  # Prostate class
-        binary_seg[seg_np > 1] = 0  # Non-prostate classes
+        image = image.permute(1, 2, 0)  # Change from (W, C, H) -> (C, H, W)
+
+        binary_seg = np.zeros_like(seg, dtype=np.uint8)
+        binary_seg[seg == 5] = 1  # Prostate class
+        binary_seg[seg != 5] = 0  # Non-prostate classes
 
         seg = torch.tensor(binary_seg, dtype=torch.uint8)
 
         return image, seg
-
-
-if __name__ == "__main__":
-    transform = transforms.Compose(
-        [
-            transforms.Resize((256, 128)),  # Standardise size
-            transforms.ToTensor(),  # Convert to tensor, scales to [0,1], C x H x W
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],  # ImageNet RGB means
-                std=[0.229, 0.224, 0.225],  # ImageNet RGB standard deviations
-            ),
-        ]
-    )
-
-    subset_size = 100
-    batch_size = 10
-
-    train_dataset = HipMRIProstateDataset(
-        data_dir="/home/groups/comp3710/HipMRI_Study_open/keras_slices_data",
-        split="train",
-        subset_size=subset_size,
-        normImage=True,
-        categorical=True,
-    )
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-    show_examples(
-        train_dataset,
-        title="HipMRI Sample Visualization",
-        n=3,
-        save_path="train_dataset_examples.png",
-    )

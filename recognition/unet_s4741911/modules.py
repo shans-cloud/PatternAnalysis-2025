9ce0fabab4a03505bd5ca7b1af5682b1bdc9f0bb
Shannon <shannon.delapena552@gmail.com>
2025-10-28 13:17:44 +1000

    Initial commit: add UNet architecture with DoubleConv
    - Added DoubleConv class combining two 3x3 Conv2D layers with BatchNorm, ReLU and Dropout
    - Implemented full UNet model for 2D prostate MRI segmentation. It includes:
    	- Encoder (downsampling), bottleneck, and decoder (upsampling with skip connections)
    	- Outputs sigmoid-activated probability maps in the [0, 1] range for binary segmentation

---
+++ recognition/unet_s4741911/modules.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+
+
+# Double Convolution Block
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, dropout_prob=0.2):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                bias=False,  # Bias is not needed with BatchNorm, as it will be cancelled out
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                out_channels,
+                out_channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=dropout_prob),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNET(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1, features=[64, 128, 256, 512]):
+        super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Encoder part of UNET
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        # Decoder part of UNET
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2)
+            )
+            self.ups.append(DoubleConv(feature * 2, feature))
+
+        self.bottleneck = DoubleConv(features[-1], features[-1] * 2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx // 2]
+
+            if x.shape != skip_connection.shape:
+                # Resize x to match skip_connection size
+                x = TF.resize(x, size=skip_connection.shape[2:])
+
+            # Concatenate along channel dimension
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx + 1](concat_skip)
+
+        # Apply sigmoid activation so output is probability map between 0 and 1
+        x = self.final_conv(x)
+        x = torch.sigmoid(x)
+
+        return x
